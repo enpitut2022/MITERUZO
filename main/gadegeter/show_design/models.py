627dbf6940f2bab3f134eea92a6bf97850a71a0d
hiroemon-m <uvhiro1221511@au.com>
2022-12-16 17:07:45 +0900

    search2

--- main/gadegeter/show_design/models.py
+++ main/gadegeter/show_design/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
+    imageid = models.IntegerField(default = 0)
     image = models.ImageField(upload_to='media')
 
 
@@ -10,13 +11,12 @@ class Tag(models.Model):
     tagid = models.IntegerField(default = 0)
     tagname = models.CharField(max_length=100)
 
-class Tagtest(models.Model):
-    tagid = models.IntegerField(default = 0)
-    tagname = models.CharField(max_length=100)
+
 
 class ImageTag(models.Model):
     imageid = models.CharField(max_length=100)
     tagid = models.CharField(max_length=100)
+
 
 
 
--- main/gadegeter/show_design/views.py
+++ main/gadegeter/show_design/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Image,ImageTag,Tag
+from .form import FindForm
 
 def index(request):
     data = Image.objects.all()
@@ -8,4 +9,26 @@ def index(request):
         'data':data,
     }
     return render(request,'showdesign/index.html',params)
+
+def find(request):
+    if (request.method == "POST"):
+        form = FindForm(request.POST)
+        find = request.POST['find']
+        msg = str(find)
+        title = "検索結果"
+        data = Image.objects.raw((f"select * from  show_design_imagetag inner join show_design_image on show_design_imagetag.imageid = show_design_image.imageid inner join show_design_tag on show_design_imagetag.tagid = show_design_tag.tagid where show_design_tag.tagname = '{msg}'"))
+    else:
+        msg = "search words"
+        form = FindForm()
+        #data = Image.objects.all()
+        data = Image.objects.raw("select * from show_design_image")
+        title = "一覧"
+    params = {
+        'title' : title,
+        'message': msg,
+        'form':form,
+        'data':data,
+    }
+    return render(request,'showdesign/index.html',params)
+
 # Create your views here.
