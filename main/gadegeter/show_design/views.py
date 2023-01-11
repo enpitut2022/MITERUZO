01a4c713b8a104d924520c177ed28b31c189890d
yoyoinoyoi <y.mar.97p25@gmail.com>
2023-01-11 12:29:35 +0900

    レイアウトをわかりやすいように変更しました

--- main/gadegeter/show_design/views.py
+++ main/gadegeter/show_design/views.py
@@ -6,9 +6,9 @@ from .form import FindForm
 def index(request):
     msg = "search words"
     form = FindForm()
-    data = Image.objects.raw("select * from show_design_image")
-    tags = Tag.objects.raw("select * from show_design_tag")
-    desk = Desk.objects.raw("select * from show_design_desk")
+    data = Image.objects.raw("select distinct * from show_design_image")
+    tags = Tag.objects.raw("select distinct * from show_design_tag")
+    desk = Desk.objects.raw("select distinct * from show_design_desk")
     title = "一覧"
     params = {
         'title' : title,
@@ -29,14 +29,14 @@ def find(request):
         print(1111111)
         print(size)
         title = "検索結果"
-        tags = Tag.objects.raw("select * from show_design_tag")
-        desk = Desk.objects.raw("select * from show_design_desk")
-        urls = Tag.objects.raw("select * from show_design_imageurl inner join show_design_tag on show_design_imageurl.tagid = show_design_tag.tagid")
-        # data = Image.objects.raw((f"select * from  show_design_imagetag left join show_design_image on show_design_imagetag.imageid = show_design_image.imageid left join show_design_tag on show_design_imagetag.tagid = show_design_tag.tagid left join show_design_imageurl on show_design_imagetag.imageid = show_design_imageurl.imageid where show_design_tag.tagname = '{msg}'"))
+        tags = Tag.objects.raw("select distinct * from show_design_tag")
+        desk = Desk.objects.raw("select distinct * from show_design_desk")
+        urls = Tag.objects.raw("select distinct * from show_design_imageurl inner join show_design_tag on show_design_imageurl.tagid = show_design_tag.tagid")
+        # data = Image.objects.raw((f"select distinct * from  show_design_imagetag left join show_design_image on show_design_imagetag.imageid = show_design_image.imageid left join show_design_tag on show_design_imagetag.tagid = show_design_tag.tagid left join show_design_imageurl on show_design_imagetag.imageid = show_design_imageurl.imageid where show_design_tag.tagname = '{msg}'"))
         if size == "指定無し":
-            data = Image.objects.raw((f"select * from  show_design_imagetag inner join show_design_image on show_design_imagetag.imageid = show_design_image.imageid inner join show_design_tag on show_design_imagetag.tagid = show_design_tag.tagid where show_design_tag.tagname = '{msg}'"))
+            data = Image.objects.raw((f"select distinct * from  show_design_imagetag inner join show_design_image on show_design_imagetag.imageid = show_design_image.imageid inner join show_design_tag on show_design_imagetag.tagid = show_design_tag.tagid where show_design_tag.tagname = '{msg}'"))
         else:
-            data = Image.objects.raw((f"select * from  show_design_imagetag inner join show_design_image on show_design_imagetag.imageid = show_design_image.imageid inner join show_design_tag on show_design_imagetag.tagid = show_design_tag.tagid inner join show_design_desk on show_design_imagetag.imageid = show_design_desk.imageid where show_design_tag.tagname = '{msg}' and show_design_desk.sizename = '{size}'"))
+            data = Image.objects.raw((f"select distinct * from  show_design_imagetag inner join show_design_image on show_design_imagetag.imageid = show_design_image.imageid inner join show_design_tag on show_design_imagetag.tagid = show_design_tag.tagid inner join show_design_desk on show_design_imagetag.imageid = show_design_desk.imageid where show_design_tag.tagname = '{msg}' and show_design_desk.sizename = '{size}'"))
         print(data)
     params = {
         'title' : title,
