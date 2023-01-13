2be27d26c625fe32bc3093a15f95b6188dd9309d
yoyoinoyoi <y.mar.97p25@gmail.com>
2023-01-13 18:06:02 +0900

    データベースがadmin で見やすくなりました

--- main/gadegeter/show_design/models.py
+++ main/gadegeter/show_design/models.py
@@ -4,17 +4,26 @@ from django.db import models
 class Image(models.Model):
     imageid = models.IntegerField(default = 0)
     image = models.ImageField(upload_to='media')
+    def str(self):
+        a = "imageid:{}".format(self.imageid)
+        return a
 
 
 class Tag(models.Model):
     tagid = models.IntegerField(default = 0)
     tagname = models.CharField(max_length=100)
+    def str(self):
+        a = "tagname:{}".format(self.tagname)
+        return a
 
 
 
 class ImageTag(models.Model):
     imageid = models.CharField(max_length=100)
     tagid = models.CharField(max_length=100)
+    def str(self):
+        a = "imageid:{}, tagid:{}".format(self.imageid,self.tagid)
+        return a
 
 
 class ImageUrl(models.Model):
@@ -22,9 +31,15 @@ class ImageUrl(models.Model):
     imageurl = models.CharField(max_length=1000)
     tagid = models.CharField(max_length=100)
     url = models.CharField(max_length=1000)
+    def str(self):
+        a = "imageid:{}, tagid:{}".format(self.imageid,self.tagid)
+        return a
 
 
 class Desk(models.Model):
     imageid = models.CharField(max_length=100)
     sizename = models.CharField(max_length=100)
+    def str(self):
+        a = "imageid:{},sizename:{}".format(self.imageid,self.sizename)
+        return a
 
